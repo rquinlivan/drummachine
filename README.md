89e75c4e2e5af23d8ecdb293b81f3ed23fa286b3
Robert Quinlivan <robert@rquinlivan.net>
2020-03-21 12:46:46 -0500

    more tests

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ A pattern file must include the following fields:
 - `Name` (`string`):  the name of this pattern. (Metadata)
 - `Bpm` (`integer`): the tempo to play at.
 - `Instruments` (`map`): a map of instrument name to instrument definition.
-- `Patterns` (`map`): a map of position to instrument names. Defines the drum pattern.
+- `Patterns` (`map`): a map of position to an array of instrument names. Defines the drum pattern. Keys used in the array must correspond to keys in the instruments map.
 
 Instrument definition must include the following fields:
 - `Name`: a *unique* name. Used as a key  in the instrument map definition.
@@ -54,16 +54,16 @@ Instrument definition must include the following fields:
 
 The project can be extended through implementing three functions:
 
-`Player`:
+#### Player
 
 Provides a function that play an instrument. In the sample `ConsolePlayer` this entails printing to console. 
 This is the main interface for implementation.
 
-`Rest`:
+#### Rest
 
 Provides a function to implement a rest. In audio `Player`s, this will likely be a no-op.
 
-`Measure`:
+#### Measure
 
 Provides a function to implement an end of a measure. In audio `Player`s, this will likely be a no-op.
 
--- drummachine_test.go
+++ drummachine_test.go
@@ -52,10 +52,84 @@ func TestPlayEmptyDrums(t *testing.T) {
 			16: {"foo"},
 		},
 	}
+	drumsB := DrumPattern{
+		Name: "test",
+		Bpm:  100000,
+		Instruments: map[string]Instrument{
+			"foo": {Name: "foo", Symbol: "?"},
+			"bar": {Name: "bar", Symbol: "("},
+		},
+		Patterns: map[int][]string{
+			1: {"foo", "bar"},
+			2: {"foo", "bar"},
+			3: {"foo", "bar"},
+			4: {"foo", "bar"},
+			5: {"foo", "bar"},
+			6: {"foo", "bar"},
+			7: {"foo", "bar"},
+			8: {"foo", "bar"},
+			9: {"foo", "bar"},
+			10: {"foo", "bar"},
+			11: {"foo", "bar"},
+			12: {"foo", "bar"},
+			13: {"foo", "bar"},
+			14: {"foo", "bar"},
+			15: {"foo", "bar"},
+			16: {"foo", "bar"},
+		},
+	}
+	drumsC := DrumPattern{
+		Name: "test",
+		Bpm:  100000,
+		Instruments: map[string]Instrument{
+			"foo": {Name: "foo", Symbol: "?"},
+			"bar": {Name: "bar", Symbol: "("},
+			"baz": {Name: "baz", Symbol: ")"},
+		},
+		Patterns: map[int][]string{
+			1: {"foo", "bar", "baz"},
+			2: {"foo", "bar", "baz"},
+			3: {"foo", "bar", "baz"},
+			4: {"foo", "bar", "baz"},
+			5: {"foo", "bar", "baz"},
+			6: {"foo", "bar", "baz"},
+			7: {"foo", "bar", "baz"},
+			8: {"foo", "bar", "baz"},
+			9: {"foo", "bar", "baz"},
+			10: {"foo", "bar", "baz"},
+			11: {"foo", "bar", "baz"},
+			12: {"foo", "bar", "baz"},
+			13: {"foo", "bar", "baz"},
+			14: {"foo", "bar", "baz"},
+			15: {"foo", "bar", "baz"},
+			16: {"foo", "bar", "baz"},
+		},
+	}
+	drumsD := DrumPattern{
+		Name: "test",
+		Bpm:  100000,
+		Instruments: map[string]Instrument{
+			"foo": {Name: "foo", Symbol: "?"},
+			"bar": {Name: "bar", Symbol: "("},
+			"baz": {Name: "baz", Symbol: ")"},
+		},
+		Patterns: map[int][]string{
+			1: {"foo"},
+		},
+	}
 	_TestPlayConfiguration(t, drumsA, 1, 16, 0, 0)
+	_TestPlayConfiguration(t, drumsB, 1, 32, 0, 0)
+	_TestPlayConfiguration(t, drumsC, 1, 48, 0, 0)
+	_TestPlayConfiguration(t, drumsD, 10, 10, 150, 9)
 	_TestPlayConfiguration(t, emptyDrums,1, 0, 16, 0)
 }
 
+// Test a configured DrumPattern
+// Params:
+// - measures: number of measures to play
+// - playCount: asserted play count
+// - restCount: asserted rest count
+// - measureCount: asserted measure break count
 func _TestPlayConfiguration(t *testing.T, pattern DrumPattern, measures int, playCount int, restCount int, measureCount int) {
 	restCalls := 0
 	measureCalls := 0
